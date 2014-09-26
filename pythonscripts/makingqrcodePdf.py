7cc3d754ac53497950d7560e39618ef70eb77345
Harish Sangireddy <harish2rb@gmail.com>
2014-09-25 22:52:37 -0500

    added qrcode scripts
    qrcode scripts

---
+++ pythonscripts/makingqrcodePdf.py
@@ -0,0 +1,54 @@
+import time
+import os
+from reportlab.lib.enums import TA_JUSTIFY
+from reportlab.lib.pagesizes import letter
+import reportlab.platypus as rptplt
+from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Image
+from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
+from reportlab.lib.units import inch
+import Image
+
+# Change this as per the location and filename you want
+pdffileName = "C:\MyStuff\UTLiftProject\qrcodeLetter.pdf"
+
+# based on the number of groups we would decide how many qrcodes we want
+# usually an 8 number of qrcodes per each group is required and this 
+# optimizes the printing options on the sticker paper
+numberofQrcodesPergroup = 8
+
+doc = SimpleDocTemplate(pdffileName,pagesize=letter,
+                        rightMargin=72,leftMargin=72,
+                        topMargin=72,bottomMargin=18)
+                        
+# create the empty document object
+Story=[]
+
+figcnt = 0
+qrcodeimagepath = "C:\\MyStuff\UTLiftProject\\FORMSQRCODE19NOV\\testPdf"
+for f in os.listdir(qrcodeimagepath):
+    print f
+    figcnt = figcnt+1
+    new_im = Image.new('RGB', (1050,400))
+    qr_file = qrcodeimagepath +'\\'+ f
+    im = Image.open(qr_file)
+    #Here I resize my opened image, so it is no bigger than 100,100
+    #im.thumbnail((210,198))
+    #Iterate through a 4 by 4 grid with spacing, to place my image
+    for i in xrange(0,1050,210):
+        for j in xrange(0,400,198):
+            #paste the image at location i,j:
+            new_im.paste(im, (i,j))
+    new_im.show()
+    new_im.save("C:\\MyStuff\\UTLiftProject\\FORMSQRCODE19NOV\\tempSaveFolder\\new_imout"+str(figcnt)+".png","png")
+
+# Now lets collect the stitched QRCode Images and append to the story doc and make the pdf
+stitchedQrcodesPath = "C:\\MyStuff\\UTLiftProject\\FORMSQRCODE19NOV\\tempSaveFolder"
+for qr in os.listdir(stitchedQrcodesPath):
+    print qr
+    qrImage = stitchedQrcodesPath+'\\'+qr
+    imAppend = rptplt.Image(qrImage,6*inch,3*inch)
+    Story.append(imAppend)
+    Story.append(Spacer(1, 2))
+    
+        
+doc.build(Story)
---
+++ pythonscripts/readQRCODExccel.py
@@ -0,0 +1,100 @@
+import gspread
+import numpy as np
+import qrcode
+import os
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+from pylab import *
+
+
+# Login with your Google account
+gc = gspread.login('biomeut', 'weareutbiometeam')
+# This is where the code will write the QRCODES initially
+defaultLocation = "C:\MyStuff\UTLiftProject\FORMSQRCODE19NOV\TUESDAY19NOV"
+
+# Open a worksheet from spreadsheet with one shot
+# This is the google spreadsheet which has the urls and the UniqueIDs
+sh = gc.open_by_url('https://docs.google.com/spreadsheet/ccc?key=0AqRKxa4BLGyDdERkcTEzbDc4WG8xNF96ZG92MmZBNGc&usp=drive_web#gid=0')
+
+# Select worksheet by index. Worksheet indexes start from zero
+worksheet = sh.get_worksheet(0)
+
+# Get all values from the first column
+values_list = worksheet.get_all_values()
+
+print values_list
+
+print np.shape(values_list)
+
+shapeOfValues_List = np.shape(values_list)
+
+
+for i in range(1,shapeOfValues_List[0]):
+    qr = qrcode.QRCode(version=1,error_correction=qrcode.constants.ERROR_CORRECT_L,box_size=10)
+    qr.add_data(values_list[i][0])
+    qr.make(fit=True)
+    img = qr.make_image()
+    
+    # Saving the image file to the default location
+    filename = values_list[i][1]#"CE341WC"+ str(i)
+    qr_file = os.path.join(defaultLocation,filename + ".jpg")
+    img_file = open(qr_file, 'wb')
+    img.save(img_file, 'JPEG')
+    img_file.close()
+    
+# Read this image
+#qr_file = "C:\MyStuff\UTLiftProject\FORMSQRCODE\G341ENV1.jpg"
+#imageqr=mpimg.imread(qr_file)
+#fig, ax = plt.subplots()
+#plt.imshow(imageqr,cmap=plt.cm.gray)
+#text(180.0, 440.0, filename)
+#plt.axis('off') # clear x- and y-axes
+#plt.show()
+#fig.savefig("C:\MyStuff\UTLiftProject\FORMSQRCODE\G341ENV1out.jpg",bbox_inches='tight')
+    
+# Read all the files in the dir where they were written initially and make eps 
+# files so that appropriate pdfs can be made and printed
+qrcodeimagepath = "C:\MyStuff\UTLiftProject\FORMSQRCODE19NOV\TUESDAY19NOV"
+j = 1
+for f in os.listdir(qrcodeimagepath):
+    print f
+    qr_file = qrcodeimagepath +'\\'+ f
+    imageqr=mpimg.imread(qr_file)
+    fig,ax = plt.subplots()
+    #fig.set_size_inches(2.0,2.0) # comment this when making eps files for printing    
+    plt.imshow(imageqr,cmap=plt.cm.gray)
+    ftemp = f.split('.')
+    #text(40.0, 480.0, ftemp[0],size = 10)
+    text(100.0, 460.0, ftemp[0],size = 30)
+    plt.axis('off')
+    #plt.show()
+    savepathepsFiles = "C:\MyStuff\UTLiftProject\FORMSQRCODE19NOV\TUESDAY19NOVEPSFIGURES"
+    fig.savefig(savepathepsFiles+'\\'+ftemp[0]+"out.eps",bbox_inches='tight')#bbox_inches='tight'
+    j = j+1
+
+
+# Read all the files in the dir where they were written initially and make PNG 
+# files so that the QRCODE has the UNIQUE ID added below it and this can be
+# used to upload on to the google forms.
+# Usually all the png files are uploaded into the biomeut folder and the qrcode
+# png is linked within the google forms
+qrcodeimagepath = "C:\MyStuff\UTLiftProject\FORMSQRCODE19NOV\TUESDAY19NOV"
+j = 1
+for f in os.listdir(qrcodeimagepath):
+    print f
+    qr_file = qrcodeimagepath +'\\'+ f
+    imageqr=mpimg.imread(qr_file)
+    fig,ax = plt.subplots()
+    fig.set_size_inches(2.0,2.0) # comment this when making eps files for printing    
+    plt.imshow(imageqr,cmap=plt.cm.gray)
+    ftemp = f.split('.')
+    #text(40.0, 480.0, ftemp[0],size = 10)
+    text(100.0, 460.0, ftemp[0],size = 30)
+    plt.axis('off')
+    #plt.show()
+    savepathepsFiles = "C:\MyStuff\UTLiftProject\FORMSQRCODE19NOV\TUESDAY19NOVPNGFIGURES"
+    fig.savefig(savepathepsFiles+'\\'+ftemp[0]+"out.png",bbox_inches='tight')#bbox_inches='tight'
+    j = j+1
+
+
+
