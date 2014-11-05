e573cdc76f8e7aaa88135f97b91a036f9e4dff4f
Harish Sangireddy <harish2rb@gmail.com>
2014-11-04 22:53:07 -0600

    added checks for crazy records
    added checks for crazy records

--- RscriptsforKrona/scriptforKrona.R
+++ RscriptsforKrona/scriptforKrona.R
@@ -16,27 +16,43 @@ qiimedata <- read.delim(qiimeinfile, header = TRUE, sep = "\t",stringsAsFactors=
 qiimecolnames <- colnames(qiimedata)
 qiimecolnames
 
-# splitting the Taxon column into K - P - C - O - F - G - S 
-# Downstream.5pm
-# variable to get
+# splitting the Taxon column into K - P - C - O - F - G
+# Assuming no species is identified in the QIIME input data
+# variable to get You can change the below to get whatever column you want to get
 varValue <- qiimedata$Downstream.5pm
 
 krondataframe <- data.frame(value=NA,Kingdom=NA,Phylum=NA,Class=NA,Order=NA,
-                             Family=NA,Genus=NA,Species=NA)
+                             Family=NA,Genus=NA)
 for (i in 1:nrow(qiimedata)){
   splittedTaxon <- unlist(strsplit(qiimedata$Taxon[i],"[;]"))
   #print(splittedTaxon)
   krondataframe <- rbind(krondataframe,c(varValue[i],
                                            splittedTaxon[1],splittedTaxon[2],
                                            splittedTaxon[3],splittedTaxon[4],
-                                           splittedTaxon[5],splittedTaxon[6],
-                                           splittedTaxon[7]))
+                                           splittedTaxon[5],splittedTaxon[6]))
   
 }
-
+krondataframe <- na.omit(krondataframe)
 # write the data.frame to a text file
+write.table(krondataframe, file = "C:\\software\\utbiomeRcode\\krondataframe.txt",
+            sep="\t",quote=FALSE, row.names = FALSE,col.names=FALSE)
+
+# Getting rid of zero value rows
+zerovaluerows <- krondataframe$value !=0
+krondataframeSanszero <- krondataframe[zerovaluerows,]
+
+# Also if any of the K,P,C,O,F,G is empty that is only e.g., k_ then put space there
+selectedRowsKingdom <- krondataframeSanszero[grep("?k__[a-zA-Z0-9]", krondataframeSanszero$Kingdom), ]
+selectedRowsphylum <- selectedRowsKingdom[grep("?p__[a-zA-Z0-9]+", selectedRowsKingdom$Phylum), ]
+selectedRowsclass <- selectedRowsphylum[grep("?c__[a-zA-Z0-9]+", selectedRowsphylum$Class), ]
+selectedRowsorder <- selectedRowsclass[grep("?o__[a-zA-Z0-9]+", selectedRowsclass$Order), ]
+selectedRowsfamily <- selectedRowsorder[grep("?f__[a-zA-Z0-9]+", selectedRowsorder$Family), ]
+selectedRowsgenus <- selectedRowsfamily[grep("?g__[a-zA-Z0-9]+", selectedRowsfamily$Genus), ]
 
 
+#write the data.frame to a text file (only non zero rows)
+write.table(krondataframeSanszero, file = "C:\\software\\utbiomeRcode\\krondataframeSanszero.txt",
+            sep="\t",quote=FALSE, row.names = FALSE,col.names=FALSE)
 
 
 
