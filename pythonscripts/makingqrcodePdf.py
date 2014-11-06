73f5c13c415957b882c28c548b0eeb20f84e94f9
Harish Sangireddy <harish2rb@gmail.com>
2014-11-06 16:54:10 -0600

    automatic creation of folders and added security
    automatic creation of folders and added security

--- pythonscripts/makingqrcodePdf.py
+++ pythonscripts/makingqrcodePdf.py
@@ -1,4 +1,9 @@
-# This makes the qrcode pdf from the QRCODEs images
+# Author: Harish Sangireddy
+# The University of Texas at Austin
+# Developed as part of the UTBIOME project
+# www.crwr-utbiome.austin.utexas.edu/utb_webapp/utbiome.html
+# This makes the qrcode pdf from the QRCODEs images and should be run after
+# the readQRCODExccel.py
 import time
 import os
 from reportlab.lib.enums import TA_JUSTIFY
@@ -7,25 +12,35 @@ import reportlab.platypus as rptplt
 from reportlab.platypus import SimpleDocTemplate, Paragraph, Spacer, Image
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
 from reportlab.lib.units import inch
-import Image
+from PIL import Image
+import shutil
 
 # Change this as per the location and filename you want
-pdffileName = "C:\MyStuff\UTLiftProject\qrcodeLetter.pdf"
+utbiomelocation = "C:\\MyStuff\\UTLiftProject\\"
 
 # based on the number of groups we would decide how many qrcodes we want
 # usually an 8 number of qrcodes per each group is required and this 
 # optimizes the printing options on the sticker paper
-numberofQrcodesPergroup = 8
+numberofQrcodesPergroup = 8 #(of repeatations per sample)
 
-doc = SimpleDocTemplate(pdffileName,pagesize=letter,
-                        rightMargin=72,leftMargin=72,
-                        topMargin=72,bottomMargin=18)
-                        
+
+# ----------------- NO NEED TO CHANGE ANYTHING BELOW ---------------
+pdffileName = utbiomelocation+"qrcodeLetter.pdf"
+doc = SimpleDocTemplate(pdffileName,pagesize=letter,\
+                        rightMargin=72,leftMargin=72,topMargin=72,bottomMargin=18)
+
 # create the empty document object
 Story=[]
 
 figcnt = 0
-qrcodeimagepath = "C:\\MyStuff\UTLiftProject\\FORMSQRCODE19NOV\\testPdf"
+qrcodeimagepath = utbiomelocation+"PNGFIGURES"
+tmpsavefolder = utbiomelocation+"tempSaveFolder"
+if os.path.exists(tmpsavefolder):
+    shutil.rmtree(tmpsavefolder)
+    os.mkdir(tmpsavefolder)
+else:
+    os.mkdir(tmpsavefolder)
+
 for f in os.listdir(qrcodeimagepath):
     print f
     figcnt = figcnt+1
@@ -40,16 +55,14 @@ for f in os.listdir(qrcodeimagepath):
             #paste the image at location i,j:
             new_im.paste(im, (i,j))
     new_im.show()
-    new_im.save("C:\\MyStuff\\UTLiftProject\\FORMSQRCODE19NOV\\tempSaveFolder\\new_imout"+str(figcnt)+".png","png")
+    new_im.save(tmpsavefolder+"\\new_imout"+str(figcnt)+".png","png")
 
 # Now lets collect the stitched QRCode Images and append to the story doc and make the pdf
-stitchedQrcodesPath = "C:\\MyStuff\\UTLiftProject\\FORMSQRCODE19NOV\\tempSaveFolder"
+stitchedQrcodesPath = tmpsavefolder
 for qr in os.listdir(stitchedQrcodesPath):
     print qr
     qrImage = stitchedQrcodesPath+'\\'+qr
     imAppend = rptplt.Image(qrImage,6*inch,3*inch)
     Story.append(imAppend)
     Story.append(Spacer(1, 2))
-    
-        
 doc.build(Story)
--- pythonscripts/readQRCODExccel.py
+++ pythonscripts/readQRCODExccel.py
@@ -1,21 +1,54 @@
+# Author: Harish Sangireddy
+# The University of Texas at Austin
+# Code written for the UTBIOME project.
 # This makes the qrcode from the google spreadsheet
 import gspread
 import numpy as np
 import qrcode
 import os
+import shutil
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 from pylab import *
 
+# Change this as per your folder structure
+utbiomelocation = "C:\\MyStuff\\UTLiftProject\\"
+print "This is a protected google spreadsheet:"
+print "Please enter UT BIOME user name and password"
+username = raw_input('user name: ')
+pwd = raw_input('password please: ')
+# -------------------- NO NEED TO CHANGE ANYTHING BELOW---------------------
+# Making and removing old files
+defaultLocation = utbiomelocation+"mainfolder"
+if os.path.exists(defaultLocation):
+    shutil.rmtree(defaultLocation)
+    os.mkdir(defaultLocation)
+else:
+    os.mkdir(defaultLocation)
+        
+qrcodeimagepath = defaultLocation
+savepathepsFiles = utbiomelocation+"EPSFIGURES"
+if os.path.exists(savepathepsFiles):
+    shutil.rmtree(savepathepsFiles)
+    os.mkdir(savepathepsFiles)
+else:
+    os.mkdir(savepathepsFiles)
+savepathpngFiles = utbiomelocation+"PNGFIGURES"
+if os.path.exists(savepathpngFiles):
+    shutil.rmtree(savepathpngFiles)
+    os.mkdir(savepathpngFiles)
+else:
+    os.mkdir(savepathpngFiles)
+googlespreadsheetUrl = "https://docs.google.com/spreadsheet/ccc?key=0AqRKxa4BLGyDdERkcTEzbDc4WG8xNF96ZG92MmZBNGc&usp=drive_web#gid=0"
 
 # Login with your Google account
-gc = gspread.login('UTbiome', 'weareutbiometeam')
+gc = gspread.login(username, pwd)
 # This is where the code will write the QRCODES initially
-defaultLocation = "C:\MyStuff\UTLiftProject\FORMSQRCODE19NOV\TUESDAY19NOV"
+#defaultLocation = "C:\MyStuff\UTLiftProject\FORMSQRCODE19NOV\TUESDAY19NOV"
 
 # Open a worksheet from spreadsheet with one shot
 # This is the google spreadsheet which has the urls and the UniqueIDs
-sh = gc.open_by_url('https://docs.google.com/spreadsheet/ccc?key=0AqRKxa4BLGyDdERkcTEzbDc4WG8xNF96ZG92MmZBNGc&usp=drive_web#gid=0')
+sh = gc.open_by_url(googlespreadsheetUrl)
 
 # Select worksheet by index. Worksheet indexes start from zero
 worksheet = sh.get_worksheet(0)
@@ -23,10 +56,8 @@ worksheet = sh.get_worksheet(0)
 # Get all values from the first column
 values_list = worksheet.get_all_values()
 
-print values_list
-
-print np.shape(values_list)
-
+#print values_list
+#print np.shape(values_list)
 shapeOfValues_List = np.shape(values_list)
 
 
@@ -43,19 +74,8 @@ for i in range(1,shapeOfValues_List[0]):
     img.save(img_file, 'JPEG')
     img_file.close()
     
-# Read this image
-#qr_file = "C:\MyStuff\UTLiftProject\FORMSQRCODE\G341ENV1.jpg"
-#imageqr=mpimg.imread(qr_file)
-#fig, ax = plt.subplots()
-#plt.imshow(imageqr,cmap=plt.cm.gray)
-#text(180.0, 440.0, filename)
-#plt.axis('off') # clear x- and y-axes
-#plt.show()
-#fig.savefig("C:\MyStuff\UTLiftProject\FORMSQRCODE\G341ENV1out.jpg",bbox_inches='tight')
-    
 # Read all the files in the dir where they were written initially and make eps 
 # files so that appropriate pdfs can be made and printed
-qrcodeimagepath = "C:\MyStuff\UTLiftProject\FORMSQRCODE19NOV\TUESDAY19NOV"
 j = 1
 for f in os.listdir(qrcodeimagepath):
     print f
@@ -63,13 +83,11 @@ for f in os.listdir(qrcodeimagepath):
     imageqr=mpimg.imread(qr_file)
     fig,ax = plt.subplots()
     #fig.set_size_inches(2.0,2.0) # comment this when making eps files for printing    
-    plt.imshow(imageqr,cmap=plt.cm.gray)
+    #plt.imshow(imageqr,cmap=plt.cm.gray)
     ftemp = f.split('.')
     #text(40.0, 480.0, ftemp[0],size = 10)
     text(100.0, 460.0, ftemp[0],size = 30)
     plt.axis('off')
-    #plt.show()
-    savepathepsFiles = "C:\MyStuff\UTLiftProject\FORMSQRCODE19NOV\TUESDAY19NOVEPSFIGURES"
     fig.savefig(savepathepsFiles+'\\'+ftemp[0]+"out.eps",bbox_inches='tight')#bbox_inches='tight'
     j = j+1
 
@@ -79,7 +97,7 @@ for f in os.listdir(qrcodeimagepath):
 # used to upload on to the google forms.
 # Usually all the png files are uploaded into the biomeut folder and the qrcode
 # png is linked within the google forms
-qrcodeimagepath = "C:\MyStuff\UTLiftProject\FORMSQRCODE19NOV\TUESDAY19NOV"
+#qrcodeimagepath = "C:\MyStuff\UTLiftProject\FORMSQRCODE19NOV\TUESDAY19NOV"
 j = 1
 for f in os.listdir(qrcodeimagepath):
     print f
@@ -87,15 +105,10 @@ for f in os.listdir(qrcodeimagepath):
     imageqr=mpimg.imread(qr_file)
     fig,ax = plt.subplots()
     fig.set_size_inches(2.0,2.0) # comment this when making eps files for printing    
-    plt.imshow(imageqr,cmap=plt.cm.gray)
+    #plt.imshow(imageqr,cmap=plt.cm.gray)
     ftemp = f.split('.')
-    #text(40.0, 480.0, ftemp[0],size = 10)
-    text(100.0, 460.0, ftemp[0],size = 30)
+    text(40.0, 480.0, ftemp[0],size = 10)
+    #text(100.0, 460.0, ftemp[0],size = 30)
     plt.axis('off')
-    #plt.show()
-    savepathepsFiles = "C:\MyStuff\UTLiftProject\FORMSQRCODE19NOV\TUESDAY19NOVPNGFIGURES"
-    fig.savefig(savepathepsFiles+'\\'+ftemp[0]+"out.png",bbox_inches='tight')#bbox_inches='tight'
+    fig.savefig(savepathpngFiles+'\\'+ftemp[0]+"out.png",bbox_inches='tight')#bbox_inches='tight'
     j = j+1
-
-
-
