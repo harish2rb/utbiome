1f4de98c3e0783192a60ef66f5670ad15e77599c
Harish Sangireddy <harish2rb@gmail.com>
2014-11-06 12:09:07 -0600

    Updated qiime to krona file scripts
    The qiime to krona file scripts, makes now automatic files.

--- RscriptsforKrona/scriptforKrona.R
+++ RscriptsforKrona/scriptforKrona.R
@@ -9,6 +9,7 @@ rm(list = ls())
 
 # qiimeinput file name
 qiimeinfile <- 'C:\\software\\utbiomeRcode\\otu_table_w_tax_rarefied7900_L6.txt'
+kronaoutfilePath <- 'C:\\software\\utbiomeRcode\\kronaoutfile\\'
 
 qiimedata <- read.delim(qiimeinfile, header = TRUE, sep = "\t",stringsAsFactors=FALSE,
                         fill=TRUE)
@@ -19,63 +20,61 @@ qiimecolnames
 # splitting the Taxon column into K - P - C - O - F - G
 # Assuming no species is identified in the QIIME input data
 # variable to get You can change the below to get whatever column you want to get
-filenametoWrite <- "Downstream_5pm.txt"
-varValue <- qiimedata$Downstream.5pm
+#filenametoWrite <- "Downstream_5pm.txt"
+#varValue <- qiimedata$Downstream.5pm
 
-krondataframe <- data.frame(value=NA,Kingdom=NA,Phylum=NA,Class=NA,Order=NA,
-                             Family=NA,Genus=NA)
-for (i in 1:nrow(qiimedata)){
-  splittedTaxon <- unlist(strsplit(qiimedata$Taxon[i],"[;]"))
-  #print(splittedTaxon)
-  krondataframe <- rbind(krondataframe,c(varValue[i],
+for (j in 2:ncol(qiimedata)){
+  # The first column is the Taxon column and we don't make a file for it
+  # We have to make a file for each column name
+  filenametoWrite <- paste0(qiimecolnames[j],".txt")
+  varValue <- qiimedata[j]
+  
+  krondataframe <- data.frame(value=NA,Kingdom=NA,Phylum=NA,Class=NA,Order=NA,
+                              Family=NA,Genus=NA)
+  for (i in 1:nrow(qiimedata)){
+    splittedTaxon <- unlist(strsplit(qiimedata$Taxon[i],"[;]"))
+    #print(splittedTaxon)
+    #print (varValue[i,])
+    krondataframe <- rbind(krondataframe,c(varValue[i,],
                                            splittedTaxon[1],splittedTaxon[2],
                                            splittedTaxon[3],splittedTaxon[4],
                                            splittedTaxon[5],splittedTaxon[6]))
+    
+  }
+  krondataframe <- na.omit(krondataframe)
+  # write the data.frame to a text file
+  #write.table(krondataframe, file = "C:\\software\\utbiomeRcode\\krondataframe.txt",
+  #            sep="\t",quote=FALSE, row.names = FALSE,col.names=FALSE)
+  
+  # Getting rid of zero value rows
+  zerovaluerows <- krondataframe$value !=0
+  krondataframeSanszero <- krondataframe[zerovaluerows,]
+  
+  # Also if any of the K,P,C,O,F,G is empty that is only e.g., k_ or other then put space there
+  krondataframeSanszero$Phylum[agrep("Other",
+                                     krondataframeSanszero$Phylum)] <- " "
+  krondataframeSanszero$Class[agrep("Other",
+                                    krondataframeSanszero$Class)] <- " "
+  krondataframeSanszero$Order[agrep("Other",
+                                    krondataframeSanszero$Order)] <- " "
+  krondataframeSanszero$Family[agrep("Other",
+                                     krondataframeSanszero$Family)] <- " "
+  krondataframeSanszero$Genus[agrep("Other",
+                                    krondataframeSanszero$Genus)] <- " "
+  
+  krondataframeSanszero$Phylum[agrep("^\\+p__$",
+                                     krondataframeSanszero$Phylum,fixed=FALSE)] <- " "
+  krondataframeSanszero$Class[agrep("^\\+c__$",
+                                    krondataframeSanszero$Class,fixed=FALSE)] <- " "
+  krondataframeSanszero$Order[agrep("^\\+o__$",
+                                    krondataframeSanszero$Order,fixed=FALSE)] <- " "
+  krondataframeSanszero$Family[agrep("^\\+f__$",
+                                     krondataframeSanszero$Family,fixed=FALSE)] <- " "
+  krondataframeSanszero$Genus[agrep("^\\+g__$",
+                                    krondataframeSanszero$Genus,fixed=FALSE)] <- " "
+  #write the data.frame to a text file (only non zero rows)
+  print (paste0("Writing file",filenametoWrite))
+  write.table(krondataframeSanszero, file = paste0(kronaoutfilePath,filenametoWrite),
+              sep="\t",quote=FALSE, row.names = FALSE,col.names=FALSE)
   
 }
-krondataframe <- na.omit(krondataframe)
-# write the data.frame to a text file
-write.table(krondataframe, file = "C:\\software\\utbiomeRcode\\krondataframe.txt",
-            sep="\t",quote=FALSE, row.names = FALSE,col.names=FALSE)
-
-# Getting rid of zero value rows
-zerovaluerows <- krondataframe$value !=0
-krondataframeSanszero <- krondataframe[zerovaluerows,]
-
-# Also if any of the K,P,C,O,F,G is empty that is only e.g., k_ or other then put space there
-krondataframeSanszero$Phylum[agrep("Other",
-                            krondataframeSanszero$Phylum)] <- " "
-krondataframeSanszero$Class[agrep("Other",
-                                   krondataframeSanszero$Class)] <- " "
-krondataframeSanszero$Order[agrep("Other",
-                                   krondataframeSanszero$Order)] <- " "
-krondataframeSanszero$Family[agrep("Other",
-                                   krondataframeSanszero$Family)] <- " "
-krondataframeSanszero$Genus[agrep("Other",
-                                   krondataframeSanszero$Genus)] <- " "
-
-krondataframeSanszero$Phylum[agrep("^\\+p__$",
-                                   krondataframeSanszero$Phylum,fixed=FALSE)] <- " "
-krondataframeSanszero$Class[agrep("^\\+c__$",
-                                  krondataframeSanszero$Class,fixed=FALSE)] <- " "
-krondataframeSanszero$Order[agrep("^\\+o__$",
-                                  krondataframeSanszero$Order,fixed=FALSE)] <- " "
-krondataframeSanszero$Family[agrep("^\\+f__$",
-                                   krondataframeSanszero$Family,fixed=FALSE)] <- " "
-krondataframeSanszero$Genus[agrep("^\\+g__$",
-                                  krondataframeSanszero$Genus,fixed=FALSE)] <- " "
-
-
-
-#write the data.frame to a text file (only non zero rows)
-write.table(krondataframeSanszero, file = paste0("C:\\software\\utbiomeRcode\\",filenametoWrite),
-            sep="\t",quote=FALSE, row.names = FALSE,col.names=FALSE)
-
-
-
-
-
-
-
-
-
--- pythonscripts/readQRCODExccel.py
+++ pythonscripts/readQRCODExccel.py
@@ -9,7 +9,7 @@ from pylab import *
 
 
 # Login with your Google account
-gc = gspread.login('utBiome', 'weareutbiometeam')
+gc = gspread.login('biomeut', 'microDna$356')
 # This is where the code will write the QRCODES initially
 defaultLocation = "C:\MyStuff\UTLiftProject\FORMSQRCODE19NOV\TUESDAY19NOV"
 
